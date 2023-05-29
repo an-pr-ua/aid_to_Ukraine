4ab50a515b6352a3109403b77057a4e064c3e8b0
an-pr-ua <andriyprokopenko71@gmail.com>
2023-05-29 23:10:15 +0300

    change path

--- src/components/BankUah.js
+++ src/components/BankUah.js
@@ -1,57 +1,68 @@
-
-function BankUah (){
-    return(
-<div className="bankuah">       
-<div className="bank">
-      <h3>UAH</h3>
-      <div className="bank-number">
-        <div>
-          <p className="bank-id">Назва компаніі</p>
-          <p className="bank-info">БЛАГОДІЙНА ОРГАНІЗАЦІЯ “БЛАГОДІЙНИЙ ФОНД “А- ХЕЛП УКРАЇНА””</p>
-          <img src="images/Vector.png"/>
+function BankUah() {
+  return (
+    <div className="bankuah">
+      <div className="bank">
+        <h3>UAH</h3>
+        <div className="bank-number">
+          <div>
+            <p className="bank-id">Назва компаніі</p>
+            <p className="bank-info">
+              БЛАГОДІЙНА ОРГАНІЗАЦІЯ “БЛАГОДІЙНИЙ ФОНД “А- ХЕЛП УКРАЇНА””
+            </p>
+            <img src="https://an-pr-ua.github.io/aid_to_Ukraine/images/Vector.png" />
+          </div>
+          <hr />
+          <div>
+            <p className="bank-id">IBAN код</p>
+            <p className="bank-info">UA643515330000026009005902082</p>
+            <img src="https://an-pr-ua.github.io/aid_to_Ukraine/images/Vector.png" />
+          </div>
+          <hr />
+          <div>
+            <p className="bank-id">Назва банку</p>
+            <p className="bank-info">АТ КБ "ПриватБанк"</p>
+            <img src="https://an-pr-ua.github.io/aid_to_Ukraine/images/Vector.png" />
+          </div>
+          <hr />
+          <div>
+            <p className="bank-id">Адреса банку</p>
+            <p className="bank-info">
+              Україна, м. Київ, вулиця Грушевського, будинок 1Д
+            </p>
+            <img src="https://an-pr-ua.github.io/aid_to_Ukraine/images/Vector.png" />
+          </div>
+          <hr />
+          <div>
+            <p className="bank-id">Код ЄДРПОУ</p>
+            <p className="bank-info">44773154</p>
+            <img src="https://an-pr-ua.github.io/aid_to_Ukraine/images/Vector.png" />
+          </div>
+          <hr />
+          <div>
+            <p className="bank-id">SWIFT код</p>
+            <p className="bank-info">PBANUA2</p>
+            <img src="https://an-pr-ua.github.io/aid_to_Ukraine/images/Vector.png" />
+          </div>
+          <hr />
+          <div>
+            <p className="bank-id">Адреса компанії</p>
+            <p className="bank-info">
+              Україна, м. Харків, вул. Валентинiвська, будинок 58А квартира
+              333-33
+            </p>
+            <img src="https://an-pr-ua.github.io/aid_to_Ukraine/images/Vector.png" />
+          </div>
+          <hr />
+          <a href="">
+            <img
+              className="copy-bank"
+              src="https://an-pr-ua.github.io/aid_to_Ukraine/images/state-layer (5).png"
+            />
+          </a>
         </div>
-        <hr/>
-        <div>
-          <p className="bank-id">IBAN код</p>
-          <p className="bank-info">UA643515330000026009005902082</p>
-          <img src="images/Vector.png"/>
-        </div>
-        <hr/>
-        <div>
-          <p className="bank-id">Назва банку</p>
-          <p className="bank-info">АТ КБ "ПриватБанк"</p>
-          <img src="images/Vector.png"/>
-        </div>
-        <hr/>
-        <div>
-          <p className="bank-id">Адреса банку</p>
-          <p className="bank-info">Україна, м. Київ, вулиця Грушевського, будинок 1Д</p>
-          <img src="images/Vector.png"/>
-        </div>
-        <hr/>
-        <div>
-          <p className="bank-id">Код ЄДРПОУ</p>
-          <p className="bank-info">44773154</p>
-          <img src="images/Vector.png"/>
-        </div>
-        <hr/>
-        <div>
-          <p className="bank-id">SWIFT код</p>
-          <p className="bank-info">PBANUA2</p>
-          <img src="images/Vector.png"/>
-        </div>
-        <hr/>
-        <div>
-          <p className="bank-id">Адреса компанії</p>
-          <p className="bank-info">Україна, м. Харків, вул. Валентинiвська, будинок 58А квартира 333-33</p>
-          <img src="images/Vector.png"/>
-        </div>
-        <hr/>
-        <a href=""><img className="copy-bank" src="images/state-layer (5).png"/></a>
       </div>
     </div>
-</div> 
-)
+  );
 }
 
 export default BankUah;
